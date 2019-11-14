Sara4994/kogito-apps

KOGITO-556 - rest server for handling process instance error

packages/management-console/src/components/Molecules/DataListItemComponent/DataListItemComponent.tsx
@@ -1,7 +1,10 @@
 import { TimeAgo } from '@n1ru4l/react-time-ago';
-import React, { useState } from 'react';
+import React, { useState, useCallback } from 'react';
 import gql from 'graphql-tag';
+import axios from 'axios';
 import {
+  Alert, 
+  AlertActionCloseButton, 
   DataListItem,
   DataListItemRow,
   DataListToggle,
@@ -14,7 +17,8 @@ import {
   KebabToggle,
   DropdownItem,
   DataListContent,
-  DropdownPosition
+  DropdownPosition,
+  Modal
 } from '@patternfly/react-core';
 import { Link } from 'react-router-dom';
 import { useApolloClient } from 'react-apollo';
@@ -27,15 +31,30 @@ export interface IOwnProps {
   parentInstanceID: string | null;
   processName: string;
   start:string;
+  state:string;
+  managementEnabled: boolean;
+  endpoint: string;
 }
 
-const DataListItemComponent: React.FC<IOwnProps> = ({ id, instanceID, instanceState, processID, parentInstanceID, processName,start }) => {
+const DataListItemComponent: React.FC<IOwnProps> = ({ id, instanceID, instanceState, state, managementEnabled, processID, endpoint, parentInstanceID, processName,start }) => {
   const [expanded, setexpanded] = useState(['kie-datalist-toggle']);
   const [isOpen, setisOpen] = useState(false);
   const [isLoaded, setisLoaded] = useState(false);
   const [isChecked, setisChecked] = useState(false);
   const [childList, setchildList] = useState([]);
+  const [openModal, setOpenModal] = useState(false);
+  const [skipAlertVisible, setSkipAlertVisible] = useState(false);
+  const [retryAlertVisible, setRetryAlertVisible] = useState(false);
+  const [error, setError] = useState('')
+  const [skipStatus, setSkipStatus] = useState('');
+  const [skipMessage, setSkipMessage] = useState('');
+  const [skipError, setSkipError] = useState(false);
+  const [retryStatus, setRetryStatus] = useState('');
+  const [retryMessage, setRetryMessage] = useState('');
+  const [retryError, setRetryError] = useState(false);
+
   const client = useApolloClient();
+  
 
   const GET_CHILD_INSTANCES = gql`
     query getChildInstances($instanceId: [String!]) {
@@ -47,9 +66,47 @@ const DataListItemComponent: React.FC<IOwnProps> = ({ id, instanceID, instanceSt
         roles
         state
         start
+        endpoint
       }
     }
   `;
+  const handleViewError = useCallback(async (_processID, _instanceID, _endpoint) => {
+    const processInstanceId = instanceID;
+    const processId = processID;
+    setOpenModal(true);
+    const result = await axios.get(`${endpoint}/management/process/${processId}/instances/${processInstanceId}/error`);
+    setError(result.data);
+  },[])
+
+  const handleSkip = useCallback(async (_processID, _instanceID, _endpoint) => {
+    const processInstanceId = instanceID;
+    const processId = processID;
+    setSkipAlertVisible(true);
+    try {
+      const result = await axios.get(`${endpoint}/management/process/${processId}/instances/${processInstanceId}/skip`);
+      setSkipStatus('success');
+      setSkipMessage(result.data);
+    }
+    catch(error) {
+      setSkipError(true)
+      setSkipMessage(JSON.stringify(error.message))
+    }
+  },[])
+  
+  const handleRetry = useCallback(async (_processID, _instanceID, _endpoint) => {
+    const processInstanceId = instanceID;
+    const processId = processID;
+    setRetryAlertVisible(true);
+    try {
+      const result = await axios.get(`${endpoint}/management/process/${processId}/instances/${processInstanceId}/retrigger`);
+      setRetryStatus('success');
+    }
+    catch(error) {
+      setRetryError(true)
+      setRetryMessage(JSON.stringify(error.message))
+    }
+    
+  },[])
   const onSelect = event => {
     setisOpen(isOpen ? false : true);
   };
@@ -61,6 +118,16 @@ const DataListItemComponent: React.FC<IOwnProps> = ({ id, instanceID, instanceSt
     setisOpen(_isOpen);
   };
 
+  const handleModalToggle = () => {
+    setOpenModal(!openModal)
+  }
+
+  const closeSkipAlert = () => {
+    setSkipAlertVisible(false)
+  }
+  const closeRetryAlert = () => {
+    setRetryAlertVisible(false)
+  }
   const toggle = async _id => {
     const index = expanded.indexOf(_id);
     const newExpanded =
@@ -77,8 +144,49 @@ const DataListItemComponent: React.FC<IOwnProps> = ({ id, instanceID, instanceSt
       setisLoaded(true);
     }
   };
+  const handleSkipButton = async () => {
+    setOpenModal(!openModal);
+    await handleSkip(processID, instanceID, endpoint);
+  }
+
+  const handleRetryButton = async () => {
+    setOpenModal(!openModal);
+    await handleRetry(processID, instanceID, endpoint);
+  }
   return (
     <React.Fragment>
+      {skipAlertVisible && skipStatus === 'success' && (
+        <Alert
+            variant="success"
+            title="Skip operation"
+            action={<AlertActionCloseButton onClose={() => closeSkipAlert()} />}
+          >
+            Process execution has successfully skipped node which was in error state.
+          </Alert>)}
+      {skipAlertVisible && skipError  && (    
+          <Alert
+          variant="danger"
+          title="Skip operation"
+          action={<AlertActionCloseButton onClose={() => closeSkipAlert()} />}
+        >
+          Process execution failed to skip node which in error state. Message: {skipMessage}
+        </Alert>)}
+        {retryAlertVisible && retryStatus === 'success' && (
+        <Alert
+            variant="success"
+            title="Retry operation"
+            action={<AlertActionCloseButton onClose={() => closeRetryAlert()} />}
+          >
+            Process execution has successfully re executed node which was in error state.
+          </Alert>)}
+      {retryAlertVisible && retryError && (    
+          <Alert
+          variant="danger"
+          title="Retry operation"
+          action={<AlertActionCloseButton onClose={() => closeRetryAlert()} />}
+        >
+          Process execution failed to re executed node which is error state. Message {retryMessage}
+        </Alert>)}
       <DataListItem aria-labelledby="kie-datalist-item" isExpanded={expanded.includes('kie-datalist-toggle')}>
         <DataListItemRow>
           <DataListToggle
@@ -100,7 +208,7 @@ const DataListItemComponent: React.FC<IOwnProps> = ({ id, instanceID, instanceSt
               <DataListCell key={1}>{processName}</DataListCell>,
               <DataListCell key={2}>
                 {start? 
-                  <TimeAgo date={new Date(`${start}`)} render={({ error, value }) => <span>{value}</span>} />
+                  <TimeAgo date={new Date(`${start}`)} render={({ _error, value }) => <span>{value}</span>} />
                 : ''}
               </DataListCell>,
               <DataListCell key={3}>{instanceState}</DataListCell>
@@ -121,16 +229,48 @@ const DataListItemComponent: React.FC<IOwnProps> = ({ id, instanceID, instanceSt
             id="kie-datalist-action"
             aria-label="Actions"
           >
-            <Dropdown
+            {state === "ERROR" && managementEnabled ? 
+              <Dropdown
               isPlain
               position={DropdownPosition.right}
               isOpen={isOpen}
               onSelect={onSelect}
               toggle={<KebabToggle onToggle={onToggle} />}
               dropdownItems={[
-                <DropdownItem key={1}>Abort</DropdownItem>,
+                <DropdownItem key={1} onClick = {() => handleRetry(processID, instanceID, endpoint)}>Retry</DropdownItem>,
+                <DropdownItem key={2} onClick = {() => handleSkip(processID, instanceID, endpoint)}>Skip</DropdownItem>,
+                <DropdownItem key={3} onClick = {() => handleViewError(processID, instanceID, endpoint) }>View Error</DropdownItem>,
               ]}
-            />
+            />:  
+            <Dropdown
+            isPlain
+            position={DropdownPosition.right}
+            isOpen={isOpen}
+            onSelect={onSelect}
+            toggle={<KebabToggle isDisabled onToggle={onToggle} />}
+            dropdownItems={[]}
+          />
+          }
+          <Modal
+          isLarge
+          title="Error"
+          isOpen={openModal}
+          onClose={handleModalToggle}
+          actions={[
+              <Button key="confirm1" variant="secondary" onClick={handleSkipButton}>
+                Skip
+              </Button>,
+              <Button key="confirm2" variant="secondary" onClick={handleRetryButton}>
+                Retry
+              </Button>,
+              <Button key="confirm3" variant="primary" onClick={handleModalToggle}>
+                Close
+              </Button>
+          ]}
+          >
+            {error}
+          </Modal>
+
           </DataListAction>
         </DataListItemRow>
         <DataListContent
@@ -151,6 +291,9 @@ const DataListItemComponent: React.FC<IOwnProps> = ({ id, instanceID, instanceSt
                   parentInstanceID={child.parentProcessInstanceId}
                   processName={child.processName}
                   start={child.start}
+                  state={child.state}
+                  managementEnabled={child.state}
+                  endpoint={child.endpoint}
                 />
               );
             })}

apache/incubator-kie-kogito-apps

packages/management-console/src/components/Molecules/DataListItemComponent/DataListItemComponent.tsx
@@ -1,7 +1,10 @@
 import { TimeAgo } from '@n1ru4l/react-time-ago';
-import React, { useState } from 'react';
+import React, { useState, useCallback } from 'react';
 import gql from 'graphql-tag';
+import axios from 'axios';
 import {
+  Alert, 
+  AlertActionCloseButton, 
   DataListItem,
   DataListItemRow,
   DataListToggle,
@@ -14,7 +17,8 @@ import {
   KebabToggle,
   DropdownItem,
   DataListContent,
-  DropdownPosition
+  DropdownPosition,
+  Modal
 } from '@patternfly/react-core';
 import { Link } from 'react-router-dom';
 import { useApolloClient } from 'react-apollo';
@@ -27,14 +31,24 @@ export interface IOwnProps {
   parentInstanceID: string | null;
   processName: string;
   start:string;
+  state:string;
+  managementEnabled: boolean;
+  endpoint: string;
 }
 
-const DataListItemComponent: React.FC<IOwnProps> = ({ id, instanceID, instanceState, processID, parentInstanceID, processName,start }) => {
+const DataListItemComponent: React.FC<IOwnProps> = ({ id, instanceID, instanceState, state, managementEnabled, processID, endpoint, parentInstanceID, processName,start }) => {
   const [expanded, setexpanded] = useState(['kie-datalist-toggle']);
   const [isOpen, setisOpen] = useState(false);
   const [isLoaded, setisLoaded] = useState(false);
   const [isChecked, setisChecked] = useState(false);
   const [childList, setchildList] = useState([]);
+  const [openModal, setOpenModal] = useState(false);
+  const [alertVisible, setAlertVisible] = useState(false);
+  const [error, setError] = useState('');
+  const [alertTitle, setAlertTitle] = useState('');
+  const [alertType, setAlertType] = useState(null);
+  const [alertMessage, setAlertMessage] = useState('');
+
   const client = useApolloClient();
 
   const GET_CHILD_INSTANCES = gql`
@@ -47,9 +61,55 @@ const DataListItemComponent: React.FC<IOwnProps> = ({ id, instanceID, instanceSt
         roles
         state
         start
+        endpoint
       }
     }
   `;
+  const handleViewError = useCallback(async (_processID, _instanceID, _endpoint) => {
+    const processInstanceId = instanceID;
+    const processId = processID;
+    setOpenModal(true);
+    const result = await axios.get(`${endpoint}/management/process/${processId}/instances/${processInstanceId}/error`);
+    setError(result.data);
+  },[])
+
+  const handleSkip = useCallback(async (_processID, _instanceID, _endpoint) => {
+    const processInstanceId = instanceID;
+    const processId = processID;
+    
+    try {
+      const result = await axios.get(`${endpoint}/management/process/${processId}/instances/${processInstanceId}/skip`);
+      setAlertTitle('Skip operation');
+      setAlertType('success');
+      setAlertMessage('Process execution has successfully skipped node which was in error state.');
+      setAlertVisible(true);
+    }
+    catch(error) {
+      setAlertTitle('Skip operation');
+      setAlertType('danger');
+      setAlertMessage('Process execution failed to skip node which in error state. Message: ' + JSON.stringify(error.message));
+      setAlertVisible(true);
+    }
+  },[])
+  
+  const handleRetry = useCallback(async (_processID, _instanceID, _endpoint) => {
+    const processInstanceId = instanceID;
+    const processId = processID;
+    try {
+      const result = await axios.get(`${endpoint}/management/process/${processId}/instances/${processInstanceId}/retrigger`);
+      setAlertTitle('Retry operation');
+      setAlertType('success');
+      setAlertMessage('Process execution has successfully re executed node which was in error state.');
+      setAlertVisible(true);
+    }
+    catch(error) {
+      setAlertTitle('Retry operation');
+      setAlertType('danger');
+      setAlertMessage('Process execution failed to re executed node which is error state. Message: ' + JSON.stringify(error.message));
+      setAlertVisible(true);
+    }
+    
+  },[])
   const onSelect = event => {
     setisOpen(isOpen ? false : true);
   };
@@ -61,6 +121,14 @@ const DataListItemComponent: React.FC<IOwnProps> = ({ id, instanceID, instanceSt
     setisOpen(_isOpen);
   };
 
+  const handleModalToggle = () => {
+    setOpenModal(!openModal)
+  }
+
+  const closeAlert = () => {
+    setAlertVisible(false);
+  }
+  
   const toggle = async _id => {
     const index = expanded.indexOf(_id);
     const newExpanded =
@@ -77,8 +145,26 @@ const DataListItemComponent: React.FC<IOwnProps> = ({ id, instanceID, instanceSt
       setisLoaded(true);
     }
   };
+  const handleSkipButton = async () => {
+    setOpenModal(!openModal);
+    await handleSkip(processID, instanceID, endpoint);
+  }
+
+  const handleRetryButton = async () => {
+    setOpenModal(!openModal);
+    await handleRetry(processID, instanceID, endpoint);
+  }
+  
   return (
     <React.Fragment>
+     {alertVisible && (
+        <Alert
+            variant={alertType}
+            title={alertTitle}
+            action={<AlertActionCloseButton onClose={() => closeAlert()} />}
+          >
+            {alertMessage}
+          </Alert>)}
       <DataListItem aria-labelledby="kie-datalist-item" isExpanded={expanded.includes('kie-datalist-toggle')}>
         <DataListItemRow>
           <DataListToggle
@@ -100,7 +186,7 @@ const DataListItemComponent: React.FC<IOwnProps> = ({ id, instanceID, instanceSt
               <DataListCell key={1}>{processName}</DataListCell>,
               <DataListCell key={2}>
                 {start? 
-                  <TimeAgo date={new Date(`${start}`)} render={({ error, value }) => <span>{value}</span>} />
+                  <TimeAgo date={new Date(`${start}`)} render={({ _error, value }) => <span>{value}</span>} />
                 : ''}
               </DataListCell>,
               <DataListCell key={3}>{instanceState}</DataListCell>
@@ -121,16 +207,48 @@ const DataListItemComponent: React.FC<IOwnProps> = ({ id, instanceID, instanceSt
             id="kie-datalist-action"
             aria-label="Actions"
           >
-            <Dropdown
+            {state === "ERROR" && managementEnabled ? 
+              <Dropdown
               isPlain
               position={DropdownPosition.right}
               isOpen={isOpen}
               onSelect={onSelect}
               toggle={<KebabToggle onToggle={onToggle} />}
               dropdownItems={[
-                <DropdownItem key={1}>Abort</DropdownItem>,
+                <DropdownItem key={1} onClick = {() => handleRetry(processID, instanceID, endpoint)}>Retry</DropdownItem>,
+                <DropdownItem key={2} onClick = {() => handleSkip(processID, instanceID, endpoint)}>Skip</DropdownItem>,
+                <DropdownItem key={3} onClick = {() => handleViewError(processID, instanceID, endpoint) }>View Error</DropdownItem>,
               ]}
-            />
+            />:  
+            <Dropdown
+            isPlain
+            position={DropdownPosition.right}
+            isOpen={isOpen}
+            onSelect={onSelect}
+            toggle={<KebabToggle isDisabled onToggle={onToggle} />}
+            dropdownItems={[]}
+          />
+          }
+          <Modal
+          isLarge
+          title="Error"
+          isOpen={openModal}
+          onClose={handleModalToggle}
+          actions={[
+              <Button key="confirm1" variant="secondary" onClick={handleSkipButton}>
+                Skip
+              </Button>,
+              <Button key="confirm2" variant="secondary" onClick={handleRetryButton}>
+                Retry
+              </Button>,
+              <Button key="confirm3" variant="primary" onClick={handleModalToggle}>
+                Close
+              </Button>
+          ]}
+          >
+            {error}
+          </Modal>
+
           </DataListAction>
         </DataListItemRow>
         <DataListContent
@@ -151,6 +269,9 @@ const DataListItemComponent: React.FC<IOwnProps> = ({ id, instanceID, instanceSt
                   parentInstanceID={child.parentProcessInstanceId}
                   processName={child.processName}
                   start={child.start}
+                  state={child.state}
+                  managementEnabled={child.state}
+                  endpoint={child.endpoint}
                 />
               );
             })}