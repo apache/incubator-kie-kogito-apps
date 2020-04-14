1516b5d12e032b69bcb7760f76c359686b331456
Ajay Jaganathan <ajayjegan1994@gmail.com>
2020-04-14 15:18:27 +0530

    KOGITO-1842 added link to endpoint url

--- packages/management-console/src/components/Organisms/ProcessDetails/ProcessDetails.tsx
+++ packages/management-console/src/components/Organisms/ProcessDetails/ProcessDetails.tsx
@@ -19,63 +19,18 @@ import {
   CheckCircleIcon,
   BanIcon,
   PausedIcon,
-  ErrorCircleOIcon
+  ErrorCircleOIcon,
+  ExternalLinkAltIcon
 } from '@patternfly/react-icons';
 import { Link } from 'react-router-dom';
-import { ProcessInstanceState } from '../../../graphql/types';
 import ProcessDescriptor from '../../Molecules/ProcessDescriptor/ProcessDescriptor';
+import { stateIconCreator } from '../../../utils/Utils';
 
 interface IOwnProps {
   data: any;
   from: any;
 }
 const ProcessDetails: React.FC<IOwnProps> = ({ data, from }) => {
-  const stateIconCreator = state => {
-    switch (state) {
-      case ProcessInstanceState.Active:
-        return (
-          <>
-            <OnRunningIcon className="pf-u-mr-sm" />
-            Active
-          </>
-        );
-      case ProcessInstanceState.Completed:
-        return (
-          <>
-            <CheckCircleIcon
-              className="pf-u-mr-sm"
-              color="var(--pf-global--success-color--100)"
-            />
-            Completed
-          </>
-        );
-      case ProcessInstanceState.Aborted:
-        return (
-          <>
-            <BanIcon className="pf-u-mr-sm" />
-            Aborted
-          </>
-        );
-      case ProcessInstanceState.Suspended:
-        return (
-          <>
-            <PausedIcon className="pf-u-mr-sm" />
-            Suspended
-          </>
-        );
-      case ProcessInstanceState.Error:
-        return (
-          <>
-            <ErrorCircleOIcon
-              className="pf-u-mr-sm"
-              color="var(--pf-global--danger-color--100)"
-            />
-            Error
-          </>
-        );
-    }
-  };
-
   return (
     <Card>
       <CardHeader>
@@ -108,13 +63,22 @@ const ProcessDetails: React.FC<IOwnProps> = ({ data, from }) => {
             </Text>
           </FormGroup>
           <FormGroup label="Endpoint" fieldId="endpoint">
-            {data.ProcessInstances[0].endpoint ? (
+            {data.ProcessInstances[0].serviceUrl ? (
               <Text component={TextVariants.p}>
-                {data.ProcessInstances[0].endpoint}
+                <Button
+                  component={'a'}
+                  variant={'link'}
+                  target={'_blank'}
+                  href={`${data.ProcessInstances[0].serviceUrl}`}
+                  isInline={true}
+                >
+                  {data.ProcessInstances[0].serviceUrl}
+                  {<ExternalLinkAltIcon className="pf-u-ml-xs" />}
+                </Button>
               </Text>
             ) : (
-                ''
-              )}
+              ''
+            )}
           </FormGroup>
           <FormGroup label="Start" fieldId="start">
             {data.ProcessInstances[0].start ? (
@@ -124,8 +88,8 @@ const ProcessDetails: React.FC<IOwnProps> = ({ data, from }) => {
                 </Moment>
               </Text>
             ) : (
-                ''
-              )}
+              ''
+            )}
           </FormGroup>
           {data.ProcessInstances[0].end && (
             <FormGroup label="End" fieldId="end">
@@ -141,9 +105,10 @@ const ProcessDetails: React.FC<IOwnProps> = ({ data, from }) => {
               <div>
                 <Link
                   to={{
-                   pathname: '/Process/' +
-                    data.ProcessInstances[0].parentProcessInstance.id,
-                   state: from
+                    pathname:
+                      '/Process/' +
+                      data.ProcessInstances[0].parentProcessInstance.id,
+                    state: from
                   }}
                 >
                   <Tooltip
@@ -167,7 +132,9 @@ const ProcessDetails: React.FC<IOwnProps> = ({ data, from }) => {
               {data.ProcessInstances[0].childProcessInstances.map(
                 (child, index) => (
                   <div key={child.id}>
-                    <Link to={{pathname:'/Process/' + child.id,state: from}}>
+                    <Link
+                      to={{ pathname: '/Process/' + child.id, state: from }}
+                    >
                       <Tooltip content={child.id}>
                         <Button variant="link" icon={<LevelDownAltIcon />}>
                           <ProcessDescriptor processInstanceData={child} />
