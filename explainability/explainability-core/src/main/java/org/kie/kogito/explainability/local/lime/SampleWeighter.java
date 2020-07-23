3af462e072a6bc96d1d0e0a8363ea630bd0812b9
Tommaso Teofili <tteofili@redhat.com>
2020-07-23 08:46:30 +0200

    KOGITO-2750 - moved sigma to a constant in SW

--- explainability/explainability-core/src/main/java/org/kie/kogito/explainability/local/lime/SampleWeighter.java
+++ explainability/explainability-core/src/main/java/org/kie/kogito/explainability/local/lime/SampleWeighter.java
@@ -27,13 +27,15 @@ import org.kie.kogito.explainability.utils.DataUtils;
  */
 class SampleWeighter {
 
+    private static final double SIGMA = 0.75;
+
     static double[] getSampleWeights(PredictionInput targetInput, Collection<Pair<double[], Double>> training) {
         int noOfFeatures = targetInput.getFeatures().size();
         double[] x = new double[noOfFeatures];
         Arrays.fill(x, 1);
 
         return training.stream().map(Pair::getLeft).map(
-                d -> DataUtils.euclideanDistance(x, d)).map(d -> DataUtils.exponentialSmoothingKernel(d, 0.75 *
+                d -> DataUtils.euclideanDistance(x, d)).map(d -> DataUtils.exponentialSmoothingKernel(d, SIGMA *
                 Math.sqrt(noOfFeatures))).mapToDouble(Double::doubleValue).toArray();
     }
 }
