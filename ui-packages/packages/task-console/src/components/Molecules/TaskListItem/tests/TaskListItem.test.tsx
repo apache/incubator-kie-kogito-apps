c5946ab5a55e50ebdd26049a2eb3d7747929076c
Jan Stastny <jstastny@redhat.com>
2020-08-03 17:58:47 +0200

    KOGITO-2939 Time sensitive snapshot in task-console (#362)
    Merging not to block other PR builds.
    
    (cherry picked from commit f1a34cdb1e63bfd2ec71b893944a87b31f1288cb)

--- ui-packages/packages/task-console/src/components/Molecules/TaskListItem/tests/TaskListItem.test.tsx
+++ ui-packages/packages/task-console/src/components/Molecules/TaskListItem/tests/TaskListItem.test.tsx
@@ -26,6 +26,8 @@ import {
 import TaskConsoleContext, { DefaultContext } from '../../../../context/TaskConsoleContext/TaskConsoleContext';
 import { TaskInfo } from '../../../../model/TaskInfo';
 
+Date.now = jest.fn(() => 1595109600000); // UTC 2020-07-19 00:00:00
+
 const userTaskInstance: UserTaskInstance = {
   id: '45a73767-5da3-49bf-9c40-d533c3e77ef3',
   description: null,
