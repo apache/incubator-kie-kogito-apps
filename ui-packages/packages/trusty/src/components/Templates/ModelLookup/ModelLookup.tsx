5778a78ab8687968cae67b30b83602609c805ba6
roberto emanuel <kelvah@gmail.com>
2020-08-23 23:43:22 +0200

    KOGITO-2777 Added InputData and ModelLookup routes and components

---
+++ ui-packages/packages/trusty/src/components/Templates/ModelLookup/ModelLookup.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { PageSection } from '@patternfly/react-core';
+
+const ModelLookup = () => {
+  return <PageSection>model lookup</PageSection>;
+};
+
+export default ModelLookup;
