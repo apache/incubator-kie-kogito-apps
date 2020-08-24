fdb4f4e0b1771459a596d1c0fe0f0f04da3a3a45
roberto emanuel <kelvah@gmail.com>
2020-08-24 10:58:49 +0200

    KOGITO-2777 Added useModelData hook

--- ui-packages/packages/trusty/src/components/Templates/ModelLookup/ModelLookup.tsx
+++ ui-packages/packages/trusty/src/components/Templates/ModelLookup/ModelLookup.tsx
@@ -1,8 +1,17 @@
 import React from 'react';
 import { PageSection } from '@patternfly/react-core';
+import useModelData from './useModelData';
+import { useParams } from 'react-router-dom';
+import { ExecutionRouteParams } from '../../../types';
 
 const ModelLookup = () => {
-  return <PageSection>model lookup</PageSection>;
+  const { executionId } = useParams<ExecutionRouteParams>();
+  const modelData = useModelData(executionId);
+  return (
+    <PageSection>
+      {modelData.status === 'SUCCESS' && <span>model diagram here</span>}
+    </PageSection>
+  );
 };
 
 export default ModelLookup;
