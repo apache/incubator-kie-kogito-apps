66caeca05b8fd936a1f0bc6cce041a1f7dd52ace
Ajay Jaganathan <ajayjeganathan@gmail.com>
2020-09-18 09:25:24 +0530

    KOGITO-3339 Integrate sorting in task inbox

--- ui-packages/packages/common/src/graphql/queries.tsx
+++ ui-packages/packages/common/src/graphql/queries.tsx
@@ -288,6 +288,7 @@ const GET_TASKS_FOR_USER = gql`
     $groups: [String!]
     $offset: Int
     $limit: Int
+    $orderBy: UserTaskInstanceOrderBy
   ) {
     UserTaskInstances(
       where: {
@@ -298,6 +299,7 @@ const GET_TASKS_FOR_USER = gql`
         ]
       }
       pagination: { offset: $offset, limit: $limit }
+      orderBy: $orderBy
     ) {
       id
       name
