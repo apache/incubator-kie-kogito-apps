5631242cb88863dbc0bd58e9388c7a7ede8e2c3a
Alessandro Costa <alecosta.me@gmail.com>
2020-10-15 17:57:24 +0200

    KOGITO-3208: fix keycloak in trusty service and integration tests

--- integration-tests/integration-tests-trusty-service/integration-tests-trusty-service-common/src/main/java/org/kie/kogito/it/trusty/AbstractTrustyExplainabilityEnd2EndIT.java
+++ integration-tests/integration-tests-trusty-service/integration-tests-trusty-service-common/src/main/java/org/kie/kogito/it/trusty/AbstractTrustyExplainabilityEnd2EndIT.java
@@ -20,8 +20,10 @@ import java.util.List;
 import java.util.function.BiFunction;
 
 import org.junit.jupiter.api.Test;
+import org.keycloak.representations.AccessTokenResponse;
 import org.kie.kogito.testcontainers.ExplainabilityServiceMessagingContainer;
 import org.kie.kogito.testcontainers.InfinispanContainer;
+import org.kie.kogito.testcontainers.KogitoKeycloakContainer;
 import org.kie.kogito.testcontainers.KogitoServiceContainer;
 import org.kie.kogito.testcontainers.TrustyServiceContainer;
 import org.kie.kogito.trusty.service.responses.ExecutionsResponse;
@@ -53,10 +55,36 @@ public abstract class AbstractTrustyExplainabilityEnd2EndIT {
     private static final String KAFKA_ALIAS = "kafka";
     private static final String KAFKA_BOOTSTRAP_SERVERS = KAFKA_ALIAS + ":9092";
 
+    private static final String KEYCLOAK_ALIAS = "keycloak";
+    private static final String KEYCLOAK_DB_VENDOR_VARIABLE = "DB_VENDOR";
+    private static final String KEYCLOAK_DB_VENDOR_VALUE = "h2";
+    private static final String KEYCLOAK_ACCESS_TOKEN_PATH = "/auth/realms/kogito/protocol/openid-connect/token";
+    private static final String KEYCLOAK_GRANT_TYPE_PARAM_NAME = "grant_type";
+    private static final String KEYCLOAK_GRANT_TYPE_PARAM_VALUE = "password";
+    private static final String KEYCLOAK_USERNAME_PARAM_NAME = "username";
+    private static final String KEYCLOAK_USERNAME_PARAM_VALUE = "jdoe";
+    private static final String KEYCLOAK_PASSWORD_PARAM_NAME = "password";
+    private static final String KEYCLOAK_PASSWORD_PARAM_VALUE = "jdoe";
+    private static final String KEYCLOAK_CLIENT_ID_PARAM_NAME = "client_id";
+    private static final String KEYCLOAK_CLIENT_ID_PARAM_VALUE = KogitoKeycloakContainer.CLIENT_ID;
+    private static final String KEYCLOAK_CLIENT_SECRET_PARAM_NAME = "client_secret";
+    private static final String KEYCLOAK_CLIENT_SECRET_PARAM_VALUE = KogitoKeycloakContainer.CLIENT_SECRET;
+
     private static final String KOGITO_SERVICE_ALIAS = "kogito-service";
     private static final String KOGITO_SERVICE_URL = "http://" + KOGITO_SERVICE_ALIAS + ":8080";
+    private static final List<String> KOGITO_SERVICE_PAYLOADS = List.of(
+            "{\"Driver\":{\"Age\":25,\"Points\":13},\"Violation\":{\"Type\":\"speed\",\"Actual Speed\":105,\"Speed Limit\":100}}",
+            "{\"Driver\":{\"Age\":37,\"Points\":20},\"Violation\":{\"Type\":\"speed\",\"Actual Speed\":135,\"Speed Limit\":100}}",
+            "{\"Driver\":{\"Age\":18,\"Points\": 0},\"Violation\":{\"Type\":\"speed\",\"Actual Speed\": 85,\"Speed Limit\": 70}}",
+            "{\"Driver\":{\"Age\":56,\"Points\":13},\"Violation\":{\"Type\":\"speed\",\"Actual Speed\": 35,\"Speed Limit\": 25}}",
+            "{\"Driver\":{\"Age\":40,\"Points\":13},\"Violation\":{\"Type\":\"speed\",\"Actual Speed\":215,\"Speed Limit\":120}}"
+    );
 
     private static final String TRUSTY_SERVICE_ALIAS = "trusty-service";
+    private static final String TRUSTY_SERVICE_OIDC_AUTH_SERVER_URL_VARIABLE = "QUARKUS_OIDC_AUTH_SERVER_URL";
+    private static final String TRUSTY_SERVICE_OIDC_AUTH_SERVER_URL_VALUE = "http://" + KEYCLOAK_ALIAS + ":8080/auth/realms/kogito";
+    private static final String TRUSTY_SERVICE_OIDC_CLIENT_ID_VARIABLE = "QUARKUS_OIDC_CLIENT_ID";
+    private static final String TRUSTY_SERVICE_OIDC_CLIENT_ID_VALUE = "kogito-trusty-service";
 
     private final BiFunction<String, String, KogitoServiceContainer> kogitoServiceContainerProducer;
 
@@ -65,7 +93,7 @@ public abstract class AbstractTrustyExplainabilityEnd2EndIT {
     }
 
     @Test
-    public void doTest() throws Exception {
+    public void doTest() {
         try (
                 final Network network = Network.newNetwork();
 
@@ -77,12 +105,19 @@ public abstract class AbstractTrustyExplainabilityEnd2EndIT {
                         .withNetwork(network)
                         .withNetworkAliases(KAFKA_ALIAS);
 
+                final KogitoKeycloakContainer keycloak = new KogitoKeycloakContainer()
+                        .withEnv(KEYCLOAK_DB_VENDOR_VARIABLE, KEYCLOAK_DB_VENDOR_VALUE)
+                        .withNetwork(network)
+                        .withNetworkAliases(KEYCLOAK_ALIAS);
+
                 final ExplainabilityServiceMessagingContainer explService = new ExplainabilityServiceMessagingContainer(KAFKA_BOOTSTRAP_SERVERS, EXPL_SERVICE_SAMPLES)
                         .withLogConsumer(new Slf4jLogConsumer(LOGGER))
                         .withNetwork(network)
                         .withNetworkAliases(EXPL_SERVICE_ALIAS);
 
                 final TrustyServiceContainer trustyService = new TrustyServiceContainer(INFINISPAN_SERVER_LIST, KAFKA_BOOTSTRAP_SERVERS, true)
+                        .withEnv(TRUSTY_SERVICE_OIDC_AUTH_SERVER_URL_VARIABLE, TRUSTY_SERVICE_OIDC_AUTH_SERVER_URL_VALUE)
+                        .withEnv(TRUSTY_SERVICE_OIDC_CLIENT_ID_VARIABLE, TRUSTY_SERVICE_OIDC_CLIENT_ID_VALUE)
                         .withLogConsumer(new Slf4jLogConsumer(LOGGER))
                         .withNetwork(network)
                         .withNetworkAliases(TRUSTY_SERVICE_ALIAS);
@@ -98,6 +133,9 @@ public abstract class AbstractTrustyExplainabilityEnd2EndIT {
             kafka.start();
             assertTrue(kafka.isRunning());
 
+            keycloak.start();
+            assertTrue(keycloak.isRunning());
+
             explService.start();
             assertTrue(explService.isRunning());
 
@@ -107,16 +145,22 @@ public abstract class AbstractTrustyExplainabilityEnd2EndIT {
             kogitoService.start();
             assertTrue(kogitoService.isRunning());
 
-            final List<String> jsonList = List.of(
-                    "{\"Driver\":{\"Age\":25,\"Points\":13},\"Violation\":{\"Type\":\"speed\",\"Actual Speed\":105,\"Speed Limit\":100}}",
-                    "{\"Driver\":{\"Age\":37,\"Points\":20},\"Violation\":{\"Type\":\"speed\",\"Actual Speed\":135,\"Speed Limit\":100}}",
-                    "{\"Driver\":{\"Age\":18,\"Points\": 0},\"Violation\":{\"Type\":\"speed\",\"Actual Speed\": 85,\"Speed Limit\": 70}}",
-                    "{\"Driver\":{\"Age\":56,\"Points\":13},\"Violation\":{\"Type\":\"speed\",\"Actual Speed\": 35,\"Speed Limit\": 25}}",
-                    "{\"Driver\":{\"Age\":40,\"Points\":13},\"Violation\":{\"Type\":\"speed\",\"Actual Speed\":215,\"Speed Limit\":120}}"
-            );
-            final int expectedExecutions = jsonList.size();
+            final String accessToken = given()
+                    .port(keycloak.getFirstMappedPort())
+                    .param(KEYCLOAK_GRANT_TYPE_PARAM_NAME, KEYCLOAK_GRANT_TYPE_PARAM_VALUE)
+                    .param(KEYCLOAK_USERNAME_PARAM_NAME, KEYCLOAK_USERNAME_PARAM_VALUE)
+                    .param(KEYCLOAK_PASSWORD_PARAM_NAME, KEYCLOAK_PASSWORD_PARAM_VALUE)
+                    .param(KEYCLOAK_CLIENT_ID_PARAM_NAME, KEYCLOAK_CLIENT_ID_PARAM_VALUE)
+                    .param(KEYCLOAK_CLIENT_SECRET_PARAM_NAME, KEYCLOAK_CLIENT_SECRET_PARAM_VALUE)
+                    .when()
+                    .post(KEYCLOAK_ACCESS_TOKEN_PATH)
+                    .as(AccessTokenResponse.class).getToken();
 
-            jsonList.forEach(json ->
+            assertNotNull(accessToken);
+
+            final int expectedExecutions = KOGITO_SERVICE_PAYLOADS.size();
+
+            KOGITO_SERVICE_PAYLOADS.forEach(json ->
                     given()
                             .port(kogitoService.getFirstMappedPort())
                             .contentType("application/json")
@@ -132,6 +176,7 @@ public abstract class AbstractTrustyExplainabilityEnd2EndIT {
                     .untilAsserted(() -> {
                         ExecutionsResponse executionsResponse = given()
                                 .port(trustyService.getFirstMappedPort())
+                                .auth().oauth2(accessToken)
                                 .when().get(String.format("/executions?limit=%d", expectedExecutions))
                                 .then().statusCode(200)
                                 .extract().as(ExecutionsResponse.class);
@@ -145,6 +190,7 @@ public abstract class AbstractTrustyExplainabilityEnd2EndIT {
 
                             SalienciesResponse salienciesResponse = given()
                                     .port(trustyService.getFirstMappedPort())
+                                    .auth().oauth2(accessToken)
                                     .when().get("/executions/decisions/" + executionId + "/explanations/saliencies")
                                     .then().statusCode(200)
                                     .extract().as(SalienciesResponse.class);
