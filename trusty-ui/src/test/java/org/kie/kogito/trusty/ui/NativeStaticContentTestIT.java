40682034b7c53f4b453b8bf111a4afb06f2e3440
jake <jacopo.r00ta@gmail.com>
2021-01-22 16:35:17 +0100

    rename package

--- trusty-ui/src/test/java/org/kie/kogito/trusty/ui/NativeStaticContentTestIT.java
+++ trusty-ui/src/test/java/org/kie/kogito/trusty/ui/NativeStaticContentTestIT.java
@@ -14,7 +14,7 @@
  *  limitations under the License.
  */
 
-package org.kie.ui;
+package org.kie.kogito.trusty.ui;
 
 import io.quarkus.test.junit.NativeImageTest;
 
--- trusty-ui/src/test/java/org/kie/kogito/trusty/ui/StaticContentTest.java
+++ trusty-ui/src/test/java/org/kie/kogito/trusty/ui/StaticContentTest.java
@@ -14,7 +14,7 @@
  *  limitations under the License.
  */
 
-package org.kie.ui;
+package org.kie.kogito.trusty.ui;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
