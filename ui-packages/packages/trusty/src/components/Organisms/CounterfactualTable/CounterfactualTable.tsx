d0074c69c0e6cc69f517d448b60951d7f7e99464
roberto emanuel <kelvah@gmail.com>
2021-04-08 17:32:33 +0200

    FAI-451: switched to PF composable table

--- ui-packages/packages/trusty/src/components/Organisms/CounterfactualTable/CounterfactualTable.tsx
+++ ui-packages/packages/trusty/src/components/Organisms/CounterfactualTable/CounterfactualTable.tsx
@@ -1,11 +1,11 @@
 import React, { useState } from 'react';
 import {
-  Table,
-  TableHeader,
-  TableBody,
-  headerCol,
-  IRow,
-  ICell
+  Td,
+  Thead,
+  Th,
+  Tbody,
+  TableComposable,
+  Tr
 } from '@patternfly/react-table';
 import { Button } from '@patternfly/react-core';
 import { PlusCircleIcon } from '@patternfly/react-icons';
@@ -16,72 +16,133 @@ interface CounterfactualTableProps {
 
 const CounterfactualTable = (props: CounterfactualTableProps) => {
   const { onOpenConstraints } = props;
-  const columns: (ICell | string)[] = [
-    { title: 'Data Type', cellTransforms: [headerCol()] },
+
+  const columns = [
+    'Data Type',
     'Input Constraint',
-    { title: 'Original input' },
+    'Original Input',
     'Counterfactual result'
   ];
-  const [rows, setRows] = useState<IRow[]>([
+  const [rows, setRows] = useState([
     {
-      cells: [
-        <div key={1}>
-          <div>one</div>
-          <div>two</div>
-        </div>,
-        <>
-          <Button
-            key={2}
-            variant={'link'}
-            isInline={true}
-            onClick={onOpenConstraints}
-            icon={<PlusCircleIcon />}
-          >
-            Add constraint
-          </Button>
-        </>,
-        'a',
-        'four'
-      ]
+      name: 'Credit Score',
+      typeRef: 'number',
+      value: 738,
+      components: null,
+      isFixed: true
     },
     {
-      cells: ['a', 'two', 'k', 'four']
+      name: 'Down Payment',
+      typeRef: 'number',
+      value: 70000,
+      components: null,
+      isFixed: true
     },
     {
-      cells: ['p', 'two', 'b', 'four']
+      name: 'Purchase Price',
+      typeRef: 'number',
+      value: 34000,
+      components: null,
+      isFixed: true
+    },
+    {
+      name: 'Monthly Tax Payment',
+      typeRef: 'number',
+      value: 0.2,
+      components: null,
+      isFixed: true
+    },
+    {
+      name: 'Monthly Insurance Payment',
+      typeRef: 'number',
+      value: 0.15,
+      components: null,
+      isFixed: true
     }
   ]);
+  const [areAllRowsSelected, setAreAllRowsSelected] = useState(false);
 
-  const onSelect = (
-    event: React.FormEvent<HTMLInputElement>,
-    isSelected: boolean,
-    rowId: number
-  ) => {
-    let updatedRows;
-    if (rowId === -1) {
-      updatedRows = rows.map(oneRow => {
-        oneRow.selected = isSelected;
-        return oneRow;
-      });
-    } else {
-      updatedRows = [...rows];
-      updatedRows[rowId].selected = isSelected;
-    }
-    setRows(updatedRows);
+  const onSelectAll = (event, isSelected) => {
+    setAreAllRowsSelected(isSelected);
+    setRows(
+      rows.map(row => {
+        row.isFixed = !isSelected;
+        return row;
+      })
+    );
   };
 
+  const onSelect = (event, isSelected, rowId) => {
+    setRows(
+      rows.map((row, index) => {
+        if (index === rowId) {
+          row.isFixed = !isSelected;
+        }
+        return row;
+      })
+    );
+    if (!isSelected && areAllRowsSelected) {
+      setAreAllRowsSelected(false);
+    } else if (isSelected && !areAllRowsSelected) {
+      let allSelected = true;
+      for (let i = 0; i < rows.length; i++) {
+        if (i !== rowId) {
+          if (rows[i].isFixed === true) {
+            allSelected = false;
+          }
+        }
+      }
+      if (allSelected) {
+        setAreAllRowsSelected(true);
+      }
+    }
+  };
   return (
     <>
-      <Table
-        onSelect={onSelect}
-        canSelectAll={true}
-        aria-label="Selectable Table"
-        cells={columns}
-        rows={rows}
-      >
-        <TableHeader />
-        <TableBody />
-      </Table>
+      <TableComposable aria-label="Counterfactual Table">
+        <Thead>
+          <Tr>
+            <Th
+              select={{
+                onSelect: onSelectAll,
+                isSelected: areAllRowsSelected
+              }}
+            />
+            <Th width={20}>{columns[0]}</Th>
+            <Th width={20}>{columns[1]}</Th>
+            <Th width={20}>{columns[2]}</Th>
+            <Th width={20}>{columns[3]}</Th>
+          </Tr>
+        </Thead>
+        <Tbody>
+          {rows.map((row, rowIndex) => (
+            <Tr key={rowIndex}>
+              <Td
+                key={`${rowIndex}_0`}
+                select={{
+                  rowIndex,
+                  onSelect,
+                  isSelected: !row.isFixed
+                }}
+              />
+              <Td key="input name">{row.name}</Td>
+              <Td key="input constraint">
+                <Button
+                  variant={'link'}
+                  isInline={true}
+                  onClick={onOpenConstraints}
+                  icon={<PlusCircleIcon />}
+                  isDisabled={row.isFixed}
+                >
+                  Add constraint
+                </Button>
+              </Td>
+              <Td key="input original value">{row.value}</Td>
+              <Td key="result">No available results</Td>
+            </Tr>
+          ))}
+        </Tbody>
+      </TableComposable>
     </>
   );
 };
