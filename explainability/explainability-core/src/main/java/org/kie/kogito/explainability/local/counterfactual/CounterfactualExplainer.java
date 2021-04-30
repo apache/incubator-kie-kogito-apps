d9380dfa1341ead01127f2305fab3c75f73cd868
Rui Vieira <ruidevieira@googlemail.com>
2021-04-30 20:55:41 +0100

    Accept CounterfactualResult consumers instead of CounterfactualSolution in the CF explainer
    The counterfactual intermediate result consumer takes a CounterfactualSolution as input but for
    consistency we should pass it a CounterfactualResult, since this is the type return by the explainAsync method.

--- explainability/explainability-core/src/main/java/org/kie/kogito/explainability/local/counterfactual/CounterfactualExplainer.java
+++ explainability/explainability-core/src/main/java/org/kie/kogito/explainability/local/counterfactual/CounterfactualExplainer.java
@@ -61,7 +61,7 @@ public class CounterfactualExplainer implements LocalExplainer<CounterfactualRes
     private final SolverConfig solverConfig;
     private final Executor executor;
 
-    public static final Consumer<CounterfactualSolution> defaultIntermediateConsumer =
+    public static final Consumer<CounterfactualResult> defaultIntermediateConsumer =
             counterfactual -> logger.debug("Intermediate counterfactual: {}", counterfactual.getEntities());
 
     public static final Consumer<CounterfactualSolution> assignSolutionId =
@@ -111,6 +111,16 @@ public class CounterfactualExplainer implements LocalExplainer<CounterfactualRes
                 }).collect(Collectors.toList());
     }
 
+    private Consumer<CounterfactualSolution> createSolutionConsumer(PredictionProvider model,
+            Consumer<CounterfactualResult> consumer) {
+        return cfs -> model.predictAsync(List.of(new PredictionInput(
+                cfs.getEntities().stream().map(CounterfactualEntity::asFeature).collect(Collectors.toList())))).thenApply(
+                        predictionOutputs -> new CounterfactualResult(cfs.getEntities(), predictionOutputs,
+                                cfs.getScore().isFeasible(),
+                                cfs.getSolutionId(), cfs.getExecutionId()))
+                .thenAccept(consumer);
+    }
+
     @Override
     public CompletableFuture<CounterfactualResult> explainAsync(Prediction prediction, PredictionProvider model) {
         CounterfactualPrediction cfPrediction = (CounterfactualPrediction) prediction;
@@ -130,13 +140,13 @@ public class CounterfactualExplainer implements LocalExplainer<CounterfactualRes
             try (SolverManager<CounterfactualSolution, UUID> solverManager =
                     SolverManager.create(solverConfig, new SolverManagerConfig())) {
 
-                final Consumer<CounterfactualSolution> intermediateResultsConsumer =
+                final Consumer<CounterfactualResult> intermediateResultsConsumer =
                         cfPrediction.getIntermediateConsumer() == null ? defaultIntermediateConsumer
                                 : cfPrediction.getIntermediateConsumer();
 
                 SolverJob<CounterfactualSolution, UUID> solverJob =
                         solverManager.solveAndListen(executionId, initial,
-                                assignSolutionId.andThen(intermediateResultsConsumer),
+                                assignSolutionId.andThen(createSolutionConsumer(model, intermediateResultsConsumer)),
                                 null);
                 try {
                     // Wait until the solving ends
