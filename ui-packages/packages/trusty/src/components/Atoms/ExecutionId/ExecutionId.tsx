0fb46cd7b17ab16e3f4fa98832e2fc3724edb5d5
roberto emanuel <kelvah@gmail.com>
2021-05-19 18:05:28 +0200

    FAI-489: displaying a shortened version of execution ids

---
+++ ui-packages/packages/trusty/src/components/Atoms/ExecutionId/ExecutionId.tsx
@@ -0,0 +1,20 @@
+import React, { useMemo } from 'react';
+import './ExecutionId.scss';
+
+type ExecutionIdProps = {
+  id: string;
+};
+
+const ExecutionId = ({ id }: ExecutionIdProps) => {
+  const shortenedId = useMemo(() => {
+    return id.substring(0, 8);
+  }, [id]);
+
+  return (
+    <span className="execution-id" style={{}}>
+      #{shortenedId}
+    </span>
+  );
+};
+
+export default ExecutionId;
