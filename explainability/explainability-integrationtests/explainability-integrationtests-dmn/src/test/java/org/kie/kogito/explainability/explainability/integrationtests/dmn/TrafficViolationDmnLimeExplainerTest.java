03b284746c8bf5c893d883d04d7b048e63cf685e
Tommaso Teofili <tteofili@redhat.com>
2021-08-25 11:17:15 +0200

    FAI-547 - dropped time limit for weighted stability, dropped sample param in pmml regression IT

--- explainability/explainability-integrationtests/explainability-integrationtests-dmn/src/test/java/org/kie/kogito/explainability/explainability/integrationtests/dmn/TrafficViolationDmnLimeExplainerTest.java
+++ explainability/explainability-integrationtests/explainability-integrationtests-dmn/src/test/java/org/kie/kogito/explainability/explainability/integrationtests/dmn/TrafficViolationDmnLimeExplainerTest.java
@@ -154,7 +154,6 @@ class TrafficViolationDmnLimeExplainerTest {
         LimeConfigOptimizer limeConfigOptimizer = new LimeConfigOptimizer()
                 .withSampling(false)
                 .withWeighting(false)
-                .withTimeLimit(60)
                 .withWeightedStability(0.4, 0.6);
 
         Random random = new Random();
