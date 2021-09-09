2ee32c462bd0d25a8116e5db17004682caa9a1c9
roberto emanuel <kelvah@gmail.com>
2021-09-09 13:14:24 +0200

    FAI-452: updated env variable configuration

--- ui-packages/packages/trusty/webpack.dev.js
+++ ui-packages/packages/trusty/webpack.dev.js
@@ -1,46 +1,36 @@
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
-const webpack = require('webpack');
 
 const HOST = process.env.HOST || 'localhost';
 const PORT = process.env.PORT || '9000';
 
-module.exports = env => {
-  return merge(common, {
-    mode: 'development',
-    devtool: 'source-map',
-    devServer: {
-      contentBase: './dist',
-      host: HOST,
-      port: PORT,
-      compress: true,
-      inline: true,
-      historyApiFallback: true,
-      hot: true,
-      overlay: true,
-      open: true
-    },
-    plugins: [
-      new webpack.EnvironmentPlugin({
-        KOGITO_TRUSTY_COUNTERFACTUAL:
-          env && env.includes('enableCF') ? 'enabled' : 'disabled'
-      })
-    ],
-    module: {
-      rules: [
-        {
-          test: /\.css$/,
-
-          use: ['style-loader', 'css-loader']
-        },
-        {
-          test: /\.s[ac]ss$/i,
-          use: ['style-loader', 'css-loader', 'sass-loader']
-        }
-      ]
-    },
-    resolve: {
-      extensions: ['.tsx', '.ts', '.js', '.jsx']
-    }
-  });
-};
+module.exports = merge(common, {
+  mode: 'development',
+  devtool: 'source-map',
+  devServer: {
+    contentBase: './dist',
+    host: HOST,
+    port: PORT,
+    compress: true,
+    inline: true,
+    historyApiFallback: true,
+    hot: true,
+    overlay: true,
+    open: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: ['style-loader', 'css-loader', 'sass-loader']
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js', '.jsx']
+  }
+});
