7434a54e29b16be3d5217e520d4ce3b0bc2677a7
Pere Fernàndez <pere.fernandez@gmail.com>
2021-10-14 15:46:03 +0200

    - Test fix

--- ui-packages/packages/task-form/src/envelope/components/CustomTaskFormDisplayer/tests/CustomTaskFormDisplayer.test.tsx
+++ ui-packages/packages/task-form/src/envelope/components/CustomTaskFormDisplayer/tests/CustomTaskFormDisplayer.test.tsx
@@ -53,6 +53,8 @@ jest.mock('@kogito-apps/components-common', () => ({
 
 jest.mock('../../FormFooter/FormFooter');
 
+Date.now = jest.fn(() => 1592000000000);
+
 const userTaskInstance: UserTaskInstance = {
   id: '45a73767-5da3-49bf-9c40-d533c3e77ef3',
   description: null,
@@ -68,14 +70,14 @@ const userTaskInstance: UserTaskInstance = {
   adminGroups: [],
   adminUsers: [],
   completed: null,
-  started: new Date('2020-02-19T11:11:56.282Z'),
+  started: new Date(Date.now()),
   excludedUsers: [],
   potentialGroups: [],
   potentialUsers: [],
   inputs:
     '{"Skippable":"true","trip":{"city":"Boston","country":"US","begin":"2020-02-19T23:00:00.000+01:00","end":"2020-02-26T23:00:00.000+01:00","visaRequired":true},"TaskName":"VisaApplication","NodeName":"Apply for visa","traveller":{"firstName":"Rachel","lastName":"White","email":"rwhite@gorle.com","nationality":"Polish","address":{"street":"Cabalone","city":"Zerf","zipCode":"765756","country":"Poland"}},"Priority":"1"}',
   outputs: '{}',
-  lastUpdate: new Date('2020-02-19T11:11:56.282Z'),
+  lastUpdate: new Date(Date.now()),
   endpoint:
     'http://localhost:8080/travels/9ae7ce3b-d49c-4f35-b843-8ac3d22fa427/VisaApplication/45a73767-5da3-49bf-9c40-d533c3e77ef3'
 };
@@ -92,7 +94,7 @@ export const customForm: CustomForm = {
   formInfo: {
     type: FormType.HTML,
     name: 'travels_VisaApplication',
-    lastModified: new Date(2021, 9, 12)
+    lastModified: new Date(Date.now())
   },
   source: '<div></div>',
   configuration: {
