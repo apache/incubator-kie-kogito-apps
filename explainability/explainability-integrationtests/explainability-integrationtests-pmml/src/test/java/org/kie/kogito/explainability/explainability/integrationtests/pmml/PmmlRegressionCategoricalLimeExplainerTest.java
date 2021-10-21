33381386089568f1988034e0f24f407020835317
mariofusco <mario.fusco@gmail.com>
2021-10-21 15:59:15 +0200

    code format

--- explainability/explainability-integrationtests/explainability-integrationtests-pmml/src/test/java/org/kie/kogito/explainability/explainability/integrationtests/pmml/PmmlRegressionCategoricalLimeExplainerTest.java
+++ explainability/explainability-integrationtests/explainability-integrationtests-pmml/src/test/java/org/kie/kogito/explainability/explainability/integrationtests/pmml/PmmlRegressionCategoricalLimeExplainerTest.java
@@ -67,7 +67,6 @@ class PmmlRegressionCategoricalLimeExplainerTest {
         categoricalVariableRegressionRuntime = getPMMLRuntime(ResourceReaderUtils.getResourceAsFile("categoricalvariablesregression/categoricalVariablesRegression.pmml"));
     }
 
-
     @Disabled("See KOGITO-6154")
     @Test
     void testPMMLRegressionCategorical() throws Exception {
