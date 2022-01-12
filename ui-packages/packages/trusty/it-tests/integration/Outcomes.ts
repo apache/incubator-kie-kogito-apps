544749f981077419770bc1a5903ffabbbc12ea42
Sona Mala <smala@redhat.com>
2022-01-12 17:55:23 +0100

    FAI-596: update the licence header (year) and the name of test suite

--- ui-packages/packages/trusty/it-tests/integration/Outcomes.ts
+++ ui-packages/packages/trusty/it-tests/integration/Outcomes.ts
@@ -1,5 +1,5 @@
 /*
- * Copyright 2021 Red Hat, Inc. and/or its affiliates.
+ * Copyright 2022 Red Hat, Inc. and/or its affiliates.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-describe('Outcomes', () => {
+describe('Outcomes - verify mocked data', () => {
   beforeEach(() => {
     cy.visit('/');
   });
