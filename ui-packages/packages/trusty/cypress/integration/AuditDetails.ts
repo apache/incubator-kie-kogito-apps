4c8b8fb7391dfa6e63815dc342b687bcc99d8b5f
Sona Mala <smala@redhat.com>
2022-02-11 13:41:48 +0100

    FAI-665: implementation of review suggetions

--- ui-packages/packages/trusty/cypress/integration/AuditDetails.ts
+++ ui-packages/packages/trusty/cypress/integration/AuditDetails.ts
@@ -14,56 +14,71 @@
  * limitations under the License.
  */
 
-describe('AuditDetails - verify that ', () => {
+describe('Audit Details Header', () => {
   beforeEach(() => {
     cy.visit('/');
   });
 
-  [
-    ['strings--4979-4e03-8692-0ec45cfca6ac', ['Outcomes', 'Outcomes details', 'Input data', 'Model lookup', 'Counterfactual analysis']],
-    ['unsuppor-ted--all--stri-913ac955b90b', ['Outcome', 'Input data', 'Model lookup', 'Counterfactual analysis']]
-  ].forEach(reqId => {
-    it(`Audit Details header - ${reqId[0]}`, () => {
-      cy.ouiaId('exec-table', 'PF4/Table')
-        .ouiaId(`${reqId[0]}`, 'PF4/TableRow')
-        .ouiaId('show-detail', 'link')
-        .click();
-      const title = 'Execution #' + String(reqId[0]).substring(0, 8);
+  it(`Single Outcome`, () => {
+    checkAuditHeader('unsuppor-ted--all--stri-913ac955b90b', [
+      'Outcome',
+      'Input data',
+      'Model lookup',
+      'Counterfactual analysis'
+    ]);
+  });
 
-      cy.ouiaType('PF4/Breadcrumb')
-        .should('exist')
-        .within($nav => {
-          cy.wrap($nav)
-            .find('li>a')
-            .should('have.length', 3)
-            .within($items => {
-              expect($items.eq(0)).have.text('Audit investigation');
-              expect($items.eq(1)).have.text(title);
-              expect($items.eq(2)).have.text(reqId[1][0]);
-            });
-        });
-      cy.ouiaId('execution-header').within(() => {
-        cy.ouiaId('title')
-          .should('be.visible')
-          .should('have.text', title);
-        cy.ouiaId('status')
-          .should('be.visible')
-          .should('have.text', 'Completed');
-      });
-
-      cy.ouiaId('nav-audit-detail')
-      .should('exist')
-      .within($nav => {
-        cy.wrap($nav)
-          .find('li>a')
-          .should('have.length', reqId[1].length)
-          .within($item => {
-            for (let value = 0; value < reqId[1].length; value++) {
-              expect($item.eq(value)).have.text(reqId[1][value]);
-            }
-          });
-      });
-      cy.get('section.outcomes').should('be.visible');
-    });
+  it(`Outcomes`, () => {
+    checkAuditHeader('strings--4979-4e03-8692-0ec45cfca6ac', [
+      'Outcomes',
+      'Outcomes details',
+      'Input data',
+      'Model lookup',
+      'Counterfactual analysis'
+    ]);
   });
 });
+
+function checkAuditHeader(reqId: string, thirdNavBar: string[]) {
+  cy.ouiaId('exec-table', 'PF4/Table')
+    .ouiaId(reqId, 'PF4/TableRow')
+    .ouiaId('show-detail', 'link')
+    .click();
+  const title = 'Execution #' + String(reqId).substring(0, 8);
+
+  cy.ouiaType('PF4/Breadcrumb')
+    .should('exist')
+    .within($nav => {
+      cy.wrap($nav)
+        .find('li>a')
+        .should('have.length', 3)
+        .within($items => {
+          expect($items.eq(0)).have.text('Audit investigation');
+          expect($items.eq(1)).have.text(title);
+          expect($items.eq(2)).have.text(thirdNavBar[0]);
+        });
+    });
+  cy.ouiaId('execution-header').within(() => {
+    cy.ouiaId('title')
+      .should('be.visible')
+      .should('have.text', title);
+    cy.ouiaId('status')
+      .should('be.visible')
+      .should('have.text', 'Completed');
+  });
+
+  cy.ouiaId('nav-audit-detail')
+    .should('exist')
+    .within($nav => {
+      cy.wrap($nav)
+        .find('li>a')
+        .should('have.length', thirdNavBar.length)
+        .within($item => {
+          for (let value = 0; value < thirdNavBar.length; value++) {
+            expect($item.eq(value)).have.text(thirdNavBar[value]);
+            expect($item.eq(value)).be.visible;
+          }
+        });
+    });
+  cy.get('section.outcomes').should('be.visible');
+}
