aca4557ba5cb016c0d416cce358572113b3417e1
Rob Geada <rob@geada.net>
2022-03-01 13:03:15 +0000

    tested

--- explainability/explainability-core/src/test/java/org/kie/kogito/explainability/local/shap/ShapResultsTest.java
+++ explainability/explainability-core/src/test/java/org/kie/kogito/explainability/local/shap/ShapResultsTest.java
@@ -1,9 +1,23 @@
+/*
+ * Copyright 2022 Red Hat, Inc. and/or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.kie.kogito.explainability.local.shap;
 
 import java.util.ArrayList;
 import java.util.List;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 import org.apache.commons.math3.linear.MatrixUtils;
 import org.apache.commons.math3.linear.RealVector;
@@ -15,10 +29,13 @@ import org.kie.kogito.explainability.model.Saliency;
 import org.kie.kogito.explainability.model.Type;
 import org.kie.kogito.explainability.model.Value;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+
 class ShapResultsTest {
-    ShapResults buildShapResults(int nOutputs, int nFeatures, int scalar1, int scalar2){
-        Saliency[] saliencies= new Saliency[nOutputs];
-        for (int i=0; i<nOutputs; i++) {
+    ShapResults buildShapResults(int nOutputs, int nFeatures, int scalar1, int scalar2) {
+        Saliency[] saliencies = new Saliency[nOutputs];
+        for (int i = 0; i < nOutputs; i++) {
             List<FeatureImportance> fis = new ArrayList<>();
             for (int j = 0; j < nFeatures; j++) {
                 fis.add(new FeatureImportance(new Feature("f", Type.NUMBER, new Value(j)), i * j * scalar1));
