932be02746647bfa091e8ff4d28feed785b6dc61
Abhishek Gill <akumar074@gmail.com>
2022-09-29 16:39:11 +0530

    - Fixed integration tests for Flyway integration

--- apps-integration-tests/integration-tests-data-index-service/integration-tests-data-index-service-common/src/main/java/org/kie/kogito/index/testcontainers/DataIndexPostgreSqlContainer.java
+++ apps-integration-tests/integration-tests-data-index-service/integration-tests-data-index-service-common/src/main/java/org/kie/kogito/index/testcontainers/DataIndexPostgreSqlContainer.java
@@ -30,6 +30,9 @@ public class DataIndexPostgreSqlContainer extends AbstractDataIndexContainer {
         addEnv("QUARKUS_DATASOURCE_JDBC_URL", postgreSqlURL);
         addEnv("QUARKUS_DATASOURCE_USERNAME", username);
         addEnv("QUARKUS_DATASOURCE_PASSWORD", password);
+        addEnv("QUARKUS_FLYWAY_MIGRATE_AT_START", "true");
+        addEnv("QUARKUS_FLYWAY_BASELINE_ON_MIGRATE", "true");
+        addEnv("QUARKUS_FLYWAY_CLEAN_AT_START", "true");
     }
 
     @Override
