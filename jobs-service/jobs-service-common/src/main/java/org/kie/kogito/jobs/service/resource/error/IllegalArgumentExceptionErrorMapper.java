b0442a3a15c218f6ffbbeefba5f360ad90fb147d
Tiago Dolphine <tiagodolphine@gmail.com>
2022-10-07 18:48:22 -0300

    fix compilation issue

--- jobs-service/jobs-service-common/src/main/java/org/kie/kogito/jobs/service/resource/error/IllegalArgumentExceptionErrorMapper.java
+++ jobs-service/jobs-service-common/src/main/java/org/kie/kogito/jobs/service/resource/error/IllegalArgumentExceptionErrorMapper.java
@@ -20,7 +20,7 @@ import javax.ws.rs.ext.Provider;
 @Provider
 public class IllegalArgumentExceptionErrorMapper extends BaseErrorMapper<IllegalArgumentException> {
 
-    public IllegalArgumentExceptionErrorMapper(boolean logStackTrace) {
+    public IllegalArgumentExceptionErrorMapper() {
         super(false);
     }
 }
