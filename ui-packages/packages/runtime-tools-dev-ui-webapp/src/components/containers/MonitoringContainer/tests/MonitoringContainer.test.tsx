6aae8f176dc84925f556041686bbd8be602c037a
Msarawan <msarawan@redhat.com>
2023-01-06 20:12:43 +0530

    Test Cases added for MonitoringContainer Component

--- ui-packages/packages/runtime-tools-dev-ui-webapp/src/components/containers/MonitoringContainer/tests/MonitoringContainer.test.tsx
+++ ui-packages/packages/runtime-tools-dev-ui-webapp/src/components/containers/MonitoringContainer/tests/MonitoringContainer.test.tsx
@@ -1,10 +1,21 @@
 import React from 'react';
 import { mount } from 'enzyme';
 import MonitoringContainer from '../MonitoringContainer';
+import { Dashboard, MonitoringView } from '@kogito-apps/monitoring';
 
 describe('WebApp - MonitorinContainer tests', () => {
+  const props = {
+    dataIndexUrl: 'http://localhost:4000',
+    dashboard: Dashboard.DETAILS,
+    workflow: ''
+  };
+
   it('Snapshot test', () => {
-    const wrapper = mount(<MonitoringContainer />);
+    const wrapper = mount(
+      <MonitoringContainer>
+        <MonitoringView {...props} />
+      </MonitoringContainer>
+    );
     expect(wrapper).toMatchSnapshot();
   });
 });
