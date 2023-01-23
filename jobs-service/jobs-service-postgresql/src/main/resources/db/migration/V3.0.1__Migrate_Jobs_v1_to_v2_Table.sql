1b1a33f2b7fb9f6b399c1fb10c0a38e979f0453a
Tiago Dolphine <tiagodolphine@gmail.com>
2023-01-23 13:48:28 -0300

    update create new job details table script

--- jobs-service/jobs-service-postgresql/src/main/resources/db/migration/V3.0.1__Migrate_Jobs_v1_to_v2_Table.sql
+++ jobs-service/jobs-service-postgresql/src/main/resources/db/migration/V3.0.1__Migrate_Jobs_v1_to_v2_Table.sql
@@ -1,14 +1,14 @@
 INSERT INTO job_details (id, correlation_id, status, last_update, fire_time, retries, execution_counter, scheduled_id, priority, trigger, recipient)
-    SELECT job.id as id,
-           job.correlation_id as correlation_id,
-           job.status as status,
-           job.last_update as last_update,
-           job.fire_time as fire_time,
-           job.retries as retries,
-           job.execution_counter as execution_counter,
-           job.scheduled_id as scheduled_id,
-           job.priority as priority,
-           job.trigger as trigger,
+    SELECT job.id AS id,
+           job.correlation_id AS correlation_id,
+           job.status AS status,
+           job.last_update AS last_update,
+           job.fire_time AS fire_time,
+           job.retries AS retries,
+           job.execution_counter AS execution_counter,
+           job.scheduled_id AS scheduled_id,
+           job.priority AS priority,
+           job.trigger AS trigger,
            json_build_object('url', job.recipient ->> 'endpoint',
                              'type', 'http',
                              'method', 'POST',
@@ -16,5 +16,5 @@ INSERT INTO job_details (id, correlation_id, status, last_update, fire_time, ret
                              'queryParams', '{}'::jsonb,
                              'headers','{}'::jsonb,
                              'payload', null
-               ) as recipient
-    FROM job_details_v1 job WHERE job.id is not null;
+               ) AS recipient
+    FROM job_details_v1 job WHERE job.id IS NOT NULL;
