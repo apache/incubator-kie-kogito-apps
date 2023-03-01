fb7d29dbf553b28fdb4d80d411196f573c73a4f8
Cristiano Nicolai <570894+cristianonicolai@users.noreply.github.com>
2023-03-01 16:35:48 +1000

    Fix Trusty test

--- apps-integration-tests/integration-tests-trusty-service/integration-tests-trusty-service-common/src/main/java/org/kie/kogito/it/trusty/AbstractTrustyExplainabilityEnd2EndIT.java
+++ apps-integration-tests/integration-tests-trusty-service/integration-tests-trusty-service-common/src/main/java/org/kie/kogito/it/trusty/AbstractTrustyExplainabilityEnd2EndIT.java
@@ -67,7 +67,7 @@ public abstract class AbstractTrustyExplainabilityEnd2EndIT {
     private static final String INFINISPAN_SERVER_LIST = INFINISPAN_ALIAS + ":11222";
 
     private static final String KAFKA_ALIAS = "kafka";
-    private static final String KAFKA_BOOTSTRAP_SERVERS = KAFKA_ALIAS + ":9092";
+    private static final String KAFKA_BOOTSTRAP_SERVERS = KAFKA_ALIAS + ":29092";
 
     private static final String KEYCLOAK_ALIAS = "keycloak";
     private static final String KEYCLOAK_DB_VENDOR_VARIABLE = "DB_VENDOR";
