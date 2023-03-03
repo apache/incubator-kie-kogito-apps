2e9eaf866f354566c1ab529af7dfb4d042f88202
Tiago Dolphine <tiagodolphine@gmail.com>
2023-03-03 16:12:46 -0300

    KOGITO-8795 Use different datasources when embedded addons for data-index and job service

--- jobs-service/kogito-addons-jobs-service/kogito-addons-quarkus-jobs-service-embedded/deployment/src/main/java/org/kie/kogito/addons/quarkus/jobs/service/embedded/deployment/KogitoAddonsQuarkusJobsServiceEmbeddedProcessor.java
+++ jobs-service/kogito-addons-jobs-service/kogito-addons-quarkus-jobs-service-embedded/deployment/src/main/java/org/kie/kogito/addons/quarkus/jobs/service/embedded/deployment/KogitoAddonsQuarkusJobsServiceEmbeddedProcessor.java
@@ -15,13 +15,22 @@
  */
 package org.kie.kogito.addons.quarkus.jobs.service.embedded.deployment;
 
-import org.kie.kogito.quarkus.addons.common.deployment.KogitoCapability;
-import org.kie.kogito.quarkus.addons.common.deployment.OneOfCapabilityKogitoAddOnProcessor;
+import javax.sql.DataSource;
 
+import io.quarkus.arc.deployment.AnnotationsTransformerBuildItem;
+import io.quarkus.arc.processor.AnnotationsTransformer;
 import io.quarkus.deployment.annotations.BuildProducer;
 import io.quarkus.deployment.annotations.BuildStep;
 import io.quarkus.deployment.builditem.FeatureBuildItem;
 import io.quarkus.deployment.builditem.SystemPropertyBuildItem;
+import io.quarkus.reactive.datasource.ReactiveDataSource;
+import io.vertx.mutiny.sqlclient.SqlClient;
+import org.jboss.jandex.AnnotationTarget;
+import org.jboss.jandex.AnnotationValue;
+import org.kie.kogito.quarkus.addons.common.deployment.KogitoCapability;
+import org.kie.kogito.quarkus.addons.common.deployment.OneOfCapabilityKogitoAddOnProcessor;
+
+import static org.kie.kogito.addons.quarkus.jobs.service.embedded.runtime.DataSourceConfigSourceFactory.DATA_SOURCE_NAME;
 
 class KogitoAddonsQuarkusJobsServiceEmbeddedProcessor extends OneOfCapabilityKogitoAddOnProcessor {
 
@@ -42,5 +51,30 @@ class KogitoAddonsQuarkusJobsServiceEmbeddedProcessor extends OneOfCapabilityKog
     void buildConfiguration(BuildProducer<SystemPropertyBuildItem> systemProperties) {
         systemProperties.produce(new SystemPropertyBuildItem(SERVICE_URL, "http://${quarkus.http.host}:${quarkus.http.port}"));
         systemProperties.produce(new SystemPropertyBuildItem(JOBS_SERVICE_URL, "${" + SERVICE_URL + "}"));
+    }
+
+    @BuildStep
+    AnnotationsTransformerBuildItem transformDataSource() {
+        return new AnnotationsTransformerBuildItem(new AnnotationsTransformer() {
+
+            public boolean appliesTo(org.jboss.jandex.AnnotationTarget.Kind kind) {
+                return kind == AnnotationTarget.Kind.FIELD;
+            }
+
+            public void transform(TransformationContext context) {
+                try {
+                    if (!(context.getTarget().kind() == AnnotationTarget.Kind.FIELD)) {
+                        return;
+                    }
+                    if (SqlClient.class.isAssignableFrom(Class.forName(context.getTarget().asField().type().name().toString()))) {
+                        context.transform().add(ReactiveDataSource.class, AnnotationValue.createStringValue("value", DATA_SOURCE_NAME)).done();
+                    } else if (DataSource.class.isAssignableFrom(Class.forName(context.getTarget().asField().type().name().toString()))) {
+                        context.transform().add(io.quarkus.agroal.DataSource.class, AnnotationValue.createStringValue("value", "jobs-service")).done();
+                    }
+                } catch (ClassNotFoundException exception) {
+                    throw new RuntimeException("Error adding DataSource annotation", exception);
+                }
+            }
+        });
     }
 }
