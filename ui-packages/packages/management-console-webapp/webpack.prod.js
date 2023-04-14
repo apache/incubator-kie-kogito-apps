bedabed886130376a255c157f95a1bb50c632e56
Saravana <saravanabalajis49@gmail.com>
2023-04-14 21:55:51 +0530

    Moved dependencies from root package.json to respective packages

--- ui-packages/packages/management-console-webapp/webpack.prod.js
+++ ui-packages/packages/management-console-webapp/webpack.prod.js
@@ -1,5 +1,5 @@
 const path = require('path');
-const merge = require('webpack-merge');
+const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
 const webpack = require('webpack');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
@@ -71,7 +71,7 @@ module.exports = merge(common, {
             '../../node_modules/@kie-tools-core/editor/dist/envelope'
           )
         ],
-        loaders: ['style-loader', 'css-loader', 'sass-loader']
+        use: ['style-loader', 'css-loader','sass-loader']
       }
     ]
   }
