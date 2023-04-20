5fdf2a8d5d1424a4ec47dcf117adf952fcca8ee1
Saravana <saravanabalajis49@gmail.com>
2023-04-20 21:25:47 +0530

    Fixed Management console webapp

--- ui-packages/packages/management-console-webapp/webpack.common.js
+++ ui-packages/packages/management-console-webapp/webpack.common.js
@@ -39,43 +39,6 @@ module.exports = {
       },
       {
         test: /\.(svg|ttf|eot|woff|woff2)$/,
-        include: [
-          path.resolve('../../node_modules/patternfly/dist/fonts'),
-          path.resolve(
-            '../../node_modules/@patternfly/react-core/dist/styles/assets/fonts'
-          ),
-          path.resolve(
-            '../../node_modules/@patternfly/react-core/dist/styles/assets/pficon'
-          ),
-          path.resolve(
-            '../../node_modules/@patternfly/patternfly/assets/fonts'
-          ),
-          path.resolve(
-            '../../node_modules/@patternfly/patternfly/assets/pficon'
-          ),
-          path.resolve('./src/static/'),
-          path.resolve(
-            '../../node_modules/@kogito-apps/consoles-common/dist/static'
-          ),
-          path.resolve(
-            '../../node_modules/@kogito-apps/components-common/dist/static'
-          ),
-          path.resolve(
-            '../../node_modules/@kogito-apps/jobs-management/dist/static'
-          ),
-          path.resolve(
-            '../../node_modules/@kogito-apps/process-details/dist/static'
-          ),
-          path.resolve(
-            '../../node_modules/@kogito-apps/management-console-shared/dist/static'
-          ),
-          path.resolve(
-            '../../node_modules/@kogito-apps/components-common/dist/static'
-          ),
-          path.resolve(
-            '../../node_modules/@kogito-apps/process-list/dist/static'
-          )
-        ],
         use: {
           loader: 'file-loader',
           options: {
@@ -110,43 +73,6 @@ module.exports = {
       },
       {
         test: /\.(jpg|jpeg|png|gif)$/i,
-        include: [
-          path.resolve(__dirname, 'src'),
-          path.resolve('../../node_modules/patternfly'),
-          path.resolve(
-            '../../node_modules/@patternfly/patternfly/assets/images'
-          ),
-          path.resolve(
-            '../../node_modules/@patternfly/react-styles/css/assets/images'
-          ),
-          path.resolve(
-            '../../node_modules/@patternfly/react-core/dist/styles/assets/images'
-          ),
-          path.resolve(
-            '../../node_modules/@patternfly/react-core/node_modules/@patternfly/react-styles/css/assets/images'
-          ),
-          path.resolve(
-            '../../node_modules/@patternfly/react-table/node_modules/@patternfly/react-styles/css/assets/images'
-          ),
-          path.resolve(
-            '../../node_modules/@kogito-apps/consoles-common/dist/static'
-          ),
-          path.resolve(
-            '../../node_modules/@kogito-apps/components-common/dist/static'
-          ),
-          path.resolve(
-            '../../node_modules/@kogito-apps/jobs-management/dist/static'
-          ),
-          path.resolve(
-            '../../node_modules/@kogito-apps/process-details/dist/static'
-          ),
-          path.resolve(
-            '../../node_modules/@kogito-apps/management-console-shared/dist/static'
-          ),
-          path.resolve(
-            '../../node_modules/@kogito-apps/process-list/dist/static'
-          )
-        ],
         use: [
           {
             loader: 'url-loader',
@@ -167,17 +93,12 @@ module.exports = {
   },
   resolve: {
     extensions: ['.ts', '.tsx', '.js'],
-    modules: [
-      path.resolve('../../node_modules'),
-      path.resolve('./node_modules'),
-      path.resolve('./src')
-    ],
+    modules: [path.resolve(__dirname, 'src'), 'node_modules'],
     plugins: [
       new TsconfigPathsPlugin({
         configFile: path.resolve(__dirname, './tsconfig.json')
       })
     ],
-    symlinks: false,
     cacheWithContext: false
   }
 };
