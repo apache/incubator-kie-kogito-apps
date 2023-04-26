a70564262c8294be231c572566f1f4fc08e51036
Saravana <saravanabalajis49@gmail.com>
2023-04-26 12:30:15 +0530

    Fixed deprecation warnings on webpack

--- ui-packages/packages/management-console-webapp/webpack.prod.js
+++ ui-packages/packages/management-console-webapp/webpack.prod.js
@@ -3,13 +3,13 @@ const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
 const webpack = require('webpack');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 
 module.exports = merge(common, {
   mode: 'production',
   devtool: 'source-map',
   optimization: {
-    minimizer: [new OptimizeCSSAssetsPlugin({})]
+    minimizer: [new CssMinimizerPlugin()]
   },
   plugins: [
     new MiniCssExtractPlugin({
