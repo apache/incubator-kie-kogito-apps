5b90d92121da9f752b538912f0f08be8a46d32f7
Enrique Gonzalez Martinez <Enrique.Gonzalez.Martinez1@ibm.com>
2023-11-06 12:17:21 +0100

    make context factory independent

---
+++ data-audit/jpa/kogito-addons-data-audit-jpa-springboot/src/main/java/org/kie/kogito/app/audit/springboot/SpringbootJPAAuditDataConfiguration.java
@@ -0,0 +1,78 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.kie.kogito.app.audit.springboot;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.autoconfigure.orm.jpa.JpaProperties;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.orm.jpa.EntityManagerFactoryBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+
+@Configuration(proxyBeanMethods = false)
+public class SpringbootJPAAuditDataConfiguration {
+
+    @Bean
+    @Primary
+    public JpaProperties primaryJpaDataAuditProperties() {
+        return new JpaProperties();
+    }
+
+    @Bean(name = "JPADataAuditProperties")
+    @Qualifier("JPADataAuditProperties")
+    @ConfigurationProperties(prefix = "data-audit.spring.jpa")
+    public JpaProperties jpaDataAuditProperties() {
+        return new JpaProperties();
+    }
+
+    @Bean(name = "jpaDataAuditEntityManagerFactory")
+    @Qualifier("jpaDataAuditEntityManagerFactory")
+    public LocalContainerEntityManagerFactoryBean jpaDataAuditEntityManagerFactory(
+            DataSource dataSource,
+            @Autowired @Qualifier("JPADataAuditProperties") JpaProperties jpaProperties) {
+        EntityManagerFactoryBuilder builder = createEntityManagerFactoryBuilder(jpaProperties);
+        return builder
+                .dataSource(dataSource)
+                .mappingResources(jpaProperties.getMappingResources().toArray(String[]::new))
+                .properties(jpaProperties.getProperties())
+                .persistenceUnit("DataAuditPU")
+                .build();
+    }
+
+    private EntityManagerFactoryBuilder createEntityManagerFactoryBuilder(JpaProperties jpaProperties) {
+        JpaVendorAdapter jpaVendorAdapter = createJpaVendorAdapter(jpaProperties);
+        return new EntityManagerFactoryBuilder(jpaVendorAdapter, jpaProperties.getProperties(), null);
+    }
+
+    private JpaVendorAdapter createJpaVendorAdapter(JpaProperties jpaProperties) {
+        HibernateJpaVendorAdapter hibernate = new HibernateJpaVendorAdapter();
+        hibernate.setDatabase(jpaProperties.getDatabase());
+        hibernate.setGenerateDdl(jpaProperties.isGenerateDdl());
+        hibernate.setShowSql(jpaProperties.isShowSql());
+        hibernate.setDatabasePlatform(jpaProperties.getDatabasePlatform());
+        return hibernate;
+    }
+}
---
+++ data-audit/kogito-addons-data-audit-quarkus-subsystem/src/main/java/org/kie/kogito/app/audit/quarkus/QuarkusDataAuditMessagingEventConsumer.java
@@ -0,0 +1,80 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.kie.kogito.app.audit.quarkus;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+import javax.transaction.Transactional;
+
+import org.eclipse.microprofile.reactive.messaging.Incoming;
+import org.kie.kogito.app.audit.api.DataAuditStoreProxyService;
+import org.kie.kogito.app.audit.spi.DataAuditContextFactory;
+import org.kie.kogito.event.job.JobInstanceDataEvent;
+import org.kie.kogito.event.process.ProcessInstanceDataEvent;
+import org.kie.kogito.event.usertask.UserTaskInstanceDataEvent;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import io.smallrye.common.annotation.Blocking;
+
+import static org.kie.kogito.app.audit.api.SubsystemConstants.KOGITO_JOBS_EVENTS;
+import static org.kie.kogito.app.audit.api.SubsystemConstants.KOGITO_PROCESSINSTANCES_EVENTS;
+import static org.kie.kogito.app.audit.api.SubsystemConstants.KOGITO_USERTASKINSTANCES_EVENTS;
+
+@ApplicationScoped
+public class QuarkusDataAuditMessagingEventConsumer {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(QuarkusDataAuditMessagingEventConsumer.class);
+
+    private DataAuditStoreProxyService proxy;
+
+    @Inject
+    DataAuditContextFactory dataAuditContextFactory;
+
+    public QuarkusDataAuditMessagingEventConsumer() {
+        proxy = DataAuditStoreProxyService.newAuditStoreService();
+    }
+
+    @Incoming(KOGITO_PROCESSINSTANCES_EVENTS)
+    @Blocking
+    @Transactional
+    public void onProcessInstanceEvent(ProcessInstanceDataEvent<?> event) {
+        LOGGER.debug("Process instance consumer received ProcessInstanceDataEvent: \n{}", event);
+        proxy.storeProcessInstanceDataEvent(dataAuditContextFactory.newDataAuditContext(), event);
+
+    }
+
+    @Incoming(KOGITO_USERTASKINSTANCES_EVENTS)
+    @Blocking
+    @Transactional
+    public void onUserTaskInstanceEvent(UserTaskInstanceDataEvent<?> event) {
+        LOGGER.debug("Task instance received UserTaskInstanceDataEvent \n{}", event);
+        proxy.storeUserTaskInstanceDataEvent(dataAuditContextFactory.newDataAuditContext(), event);
+
+    }
+
+    @Incoming(KOGITO_JOBS_EVENTS)
+    @Blocking
+    @Transactional
+    public void onJobEvent(JobInstanceDataEvent event) {
+        LOGGER.debug("Job received KogitoJobCloudEvent \n{}", event);
+        proxy.storeJobDataEvent(dataAuditContextFactory.newDataAuditContext(), event);
+    }
+
+}
