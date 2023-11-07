ce48bae7ed6e7a2a68b5c79183f5713a18995473
Tiago Dolphine <tiagodolphine@gmail.com>
2023-11-07 09:14:52 -0300

    fix formatting

--- data-index/data-index-storage/data-index-storage-oracle/src/main/java/org/kie/kogito/index/oracle/storage/ProcessDefinitionEntityStorage.java
+++ data-index/data-index-storage/data-index-storage-oracle/src/main/java/org/kie/kogito/index/oracle/storage/ProcessDefinitionEntityStorage.java
@@ -74,7 +74,6 @@ public class ProcessDefinitionEntityStorage extends AbstractStorage<ProcessDefin
             return repository.findById(new ProcessDefinitionEntityId(s), lockModeType);
         }
 
-
         @Override
         public void persist(ProcessDefinitionEntity entity) {
             repository.persist(entity);
