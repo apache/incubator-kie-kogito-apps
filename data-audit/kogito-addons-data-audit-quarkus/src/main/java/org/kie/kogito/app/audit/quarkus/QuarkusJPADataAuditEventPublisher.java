d157e3a7595db61963c45d97173b22b02cbf335e
Pere Fernández <pefernan@users.noreply.github.com>
2023-12-22 13:36:59 +0100

    - fix log

--- data-audit/kogito-addons-data-audit-quarkus/src/main/java/org/kie/kogito/app/audit/quarkus/QuarkusJPADataAuditEventPublisher.java
+++ data-audit/kogito-addons-data-audit-quarkus/src/main/java/org/kie/kogito/app/audit/quarkus/QuarkusJPADataAuditEventPublisher.java
@@ -68,7 +68,7 @@ public class QuarkusJPADataAuditEventPublisher implements EventPublisher {
             proxy.storeUserTaskInstanceDataEvent(dataAuditContextFactory.newDataAuditContext(), (UserTaskInstanceDataEvent<?>) event);
             return;
         } else if (event instanceof JobInstanceDataEvent) {
-            LOGGER.info("Processing job instance event {}", event);
+            LOGGER.debug("Processing job instance event {}", event);
             proxy.storeJobDataEvent(dataAuditContextFactory.newDataAuditContext(), (JobInstanceDataEvent) event);
             return;
         }
