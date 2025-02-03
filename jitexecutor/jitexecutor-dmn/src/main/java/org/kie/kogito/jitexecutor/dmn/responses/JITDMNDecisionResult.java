2a1562e52e1e22756c4869b8bb9055d8db3e7c9a
Gabriele-Cardosi <gabriele.cardosi@ibm.com>
2025-02-03 15:05:18 +0100

    [incubator-kie-issues#1791] Moving evaluationHitIds map inside JITDMNDecisionResult. Add/modify test

--- jitexecutor/jitexecutor-dmn/src/main/java/org/kie/kogito/jitexecutor/dmn/responses/JITDMNDecisionResult.java
+++ jitexecutor/jitexecutor-dmn/src/main/java/org/kie/kogito/jitexecutor/dmn/responses/JITDMNDecisionResult.java
@@ -21,6 +21,7 @@ package org.kie.kogito.jitexecutor.dmn.responses;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 import org.kie.dmn.api.core.DMNDecisionResult;
 import org.kie.dmn.api.core.DMNMessage;
@@ -39,17 +40,20 @@ public class JITDMNDecisionResult implements Serializable,
 
     private DecisionEvaluationStatus status;
 
+    private Map<String, Integer> evaluationHitIds;
+
     public JITDMNDecisionResult() {
         // Intentionally blank.
     }
 
-    public static JITDMNDecisionResult of(DMNDecisionResult value) {
+    public static JITDMNDecisionResult of(DMNDecisionResult value, Map<String, Integer> decisionEvaluationHitIdsMap) {
         JITDMNDecisionResult res = new JITDMNDecisionResult();
         res.decisionId = value.getDecisionId();
         res.decisionName = value.getDecisionName();
         res.setResult(value.getResult());
         res.setMessages(value.getMessages());
         res.status = value.getEvaluationStatus();
+        res.evaluationHitIds = decisionEvaluationHitIdsMap;
         return res;
     }
 
@@ -98,6 +102,14 @@ public class JITDMNDecisionResult implements Serializable,
         for (DMNMessage m : messages) {
             this.messages.add(JITDMNMessage.of(m));
         }
+    }
+
+    public Map<String, Integer> getEvaluationHitIds() {
+        return evaluationHitIds;
+    }
+
+    public void setEvaluationHitIds(Map<String, Integer> evaluationHitIds) {
+        this.evaluationHitIds = evaluationHitIds;
     }
 
     @Override
