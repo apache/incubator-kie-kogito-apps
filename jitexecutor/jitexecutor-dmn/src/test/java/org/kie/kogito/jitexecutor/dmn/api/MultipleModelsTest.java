afa7c1ada49d8f268237705ba9f352f308358345
Gabriele-Cardosi <gabriele.cardosi@ibm.com>
2025-02-07 13:39:45 +0100

    [incubator-kie-issues#1805] Fix serialization/deserialization inside JITExecutor

--- jitexecutor/jitexecutor-dmn/src/test/java/org/kie/kogito/jitexecutor/dmn/api/MultipleModelsTest.java
+++ jitexecutor/jitexecutor-dmn/src/test/java/org/kie/kogito/jitexecutor/dmn/api/MultipleModelsTest.java
@@ -22,7 +22,10 @@ import java.io.IOException;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.kie.dmn.api.core.DMNDecisionResult;
@@ -38,14 +41,19 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.module.SimpleModule;
+import com.fasterxml.jackson.databind.node.ArrayNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.fasterxml.jackson.databind.type.CollectionType;
 import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
 
 import io.quarkus.test.junit.QuarkusTest;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import io.restassured.response.ResponseBody;
 
 import static io.restassured.RestAssured.given;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -54,6 +62,7 @@ import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.kie.kogito.jitexecutor.dmn.TestingUtils.getModelFromIoUtils;
+import static org.kie.kogito.jitexecutor.dmn.api.JITDMNResourceTest.EVALUATION_HIT_IDS_FIELD_NAME;
 
 @QuarkusTest
 public class MultipleModelsTest {
@@ -61,8 +70,10 @@ public class MultipleModelsTest {
 
     private static final String URI1 = "invalid_models/DMNv1_x/multiple/importing.dmn";
     private static final String URI2 = "valid_models/DMNv1_x/multiple/stdlib.dmn";
+    private static final String URI3 = "valid_models/DMNv1_5/Sample.dmn";
     private static ResourceWithURI model1;
     private static ResourceWithURI model2;
+    private static ResourceWithURI model3;
 
     private static final String XAIURI1 = "invalid_models/DMNv1_x/test.dmn";
     private static ResourceWithURI xaimodel1;
@@ -92,6 +103,7 @@ public class MultipleModelsTest {
         RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
         model1 = new ResourceWithURI(URI1, getModelFromIoUtils(URI1));
         model2 = new ResourceWithURI(URI2, getModelFromIoUtils(URI2));
+        model3 = new ResourceWithURI(URI3, getModelFromIoUtils(URI3));
         xaimodel1 = new ResourceWithURI(XAIURI1, getModelFromIoUtils(XAIURI1));
         ch11model1 = new ResourceWithURI(CH11URI1, getModelFromIoUtils(CH11URI1));
         ch11model2 = new ResourceWithURI(CH11URI2, getModelFromIoUtils(CH11URI2));
@@ -141,6 +153,57 @@ public class MultipleModelsTest {
                 .then()
                 .statusCode(200)
                 .body("dmnContext.'my decision'", is("Ciao, John Doe (age:47)."));
+    }
+
+    @Test
+    void testEvaluationHitIds() throws IOException {
+        final String ruleId0 = "_1FA12B9F-288C-42E8-B77F-BE2D3702B7B6";
+        final String ruleId1 = "_C8FA33B1-AF6E-4A59-B7B9-6FDF1F495C44";
+        Map<String, Object> context = new HashMap<>();
+        context.put("Credit Score", Map.of("FICO", 700));
+        Map<String, Object> monthly = new HashMap<>();
+        monthly.put("Income", 121233);
+        monthly.put("Repayments", 33);
+        monthly.put("Expenses", 123);
+        monthly.put("Tax", 32);
+        monthly.put("Insurance", 55);
+        Map<String, Object> applicantData = new HashMap<>();
+        applicantData.put("Age", 32);
+        applicantData.put("Marital Status", "S");
+        applicantData.put("Employment Status", "Employed");
+        applicantData.put("Monthly", monthly);
+        context.put("Applicant Data", applicantData);
+        Map<String, Object> requestedProduct = new HashMap<>();
+        requestedProduct.put("Type", "Special Loan");
+        requestedProduct.put("Rate", 1);
+        requestedProduct.put("Term", 2);
+        requestedProduct.put("Amount", 333);
+        context.put("Requested Product", requestedProduct);
+        context.put("id", "_0A185BAC-7692-45FA-B722-7C86C626BD51");
+        JITDMNPayload jitdmnpayload = new JITDMNPayload(URI3, List.of(model3), context);
+
+        Response response = given().contentType(ContentType.JSON)
+                .body(jitdmnpayload)
+                .when().post("/jitdmn/dmnresult");
+
+        ResponseBody body = response.getBody();
+        String responseString = body.asString();
+        JsonNode retrieved = MAPPER.readTree(responseString);
+        ArrayNode decisionResultsNode = (ArrayNode) retrieved.get("decisionResults");
+        Iterable<JsonNode> iterable = () -> decisionResultsNode.elements();
+        Stream<JsonNode> targetStream = StreamSupport.stream(iterable.spliterator(), false);
+        ObjectNode decisionNode = (ObjectNode) targetStream.filter(node -> node.get("decisionName").asText().equals("Credit Score Rating")).findFirst().get();
+        ObjectNode evaluationHitIdsNode = (ObjectNode) decisionNode.get(EVALUATION_HIT_IDS_FIELD_NAME);
+        Assertions.assertThat(evaluationHitIdsNode).hasSize(1);
+        Map<String, Integer> expectedEvaluationHitIds0 = Map.of(ruleId0, 1);
+        evaluationHitIdsNode.fields().forEachRemaining(entry -> Assertions.assertThat(expectedEvaluationHitIds0).containsEntry(entry.getKey(), entry.getValue().asInt()));
+
+        targetStream = StreamSupport.stream(iterable.spliterator(), false);
+        decisionNode = (ObjectNode) targetStream.filter(node -> node.get("decisionName").asText().equals("Loan Pre-Qualification")).findFirst().get();
+        evaluationHitIdsNode = (ObjectNode) decisionNode.get(EVALUATION_HIT_IDS_FIELD_NAME);
+        Assertions.assertThat(evaluationHitIdsNode).hasSize(1);
+        Map<String, Integer> expectedEvaluationHitIds1 = Map.of(ruleId1, 1);
+        evaluationHitIdsNode.fields().forEachRemaining(entry -> Assertions.assertThat(expectedEvaluationHitIds1).containsEntry(entry.getKey(), entry.getValue().asInt()));
     }
 
     @Test
