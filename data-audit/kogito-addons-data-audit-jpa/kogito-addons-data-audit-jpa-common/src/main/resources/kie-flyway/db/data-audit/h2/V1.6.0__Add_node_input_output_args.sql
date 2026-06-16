85da5dfcbadf45dc5b798a9afc9abcc24516cc3d
Deepak Joseph <deepakjoseph305@gmail.com>
2026-06-16 22:42:30 +0530

    Changed datatype for node args

--- data-audit/kogito-addons-data-audit-jpa/kogito-addons-data-audit-jpa-common/src/main/resources/kie-flyway/db/data-audit/h2/V1.6.0__Add_node_input_output_args.sql
+++ data-audit/kogito-addons-data-audit-jpa/kogito-addons-data-audit-jpa-common/src/main/resources/kie-flyway/db/data-audit/h2/V1.6.0__Add_node_input_output_args.sql
@@ -18,5 +18,5 @@
  */
 
 -- Add input_args and output_args columns to Process_Instance_Node_Log table
-ALTER TABLE Process_Instance_Node_Log ADD COLUMN input_args VARCHAR(4000);
-ALTER TABLE Process_Instance_Node_Log ADD COLUMN output_args VARCHAR(4000);
+ALTER TABLE Process_Instance_Node_Log ADD COLUMN input_args VARCHAR(MAX);
+ALTER TABLE Process_Instance_Node_Log ADD COLUMN output_args VARCHAR(MAX);
